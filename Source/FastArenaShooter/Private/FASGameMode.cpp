e31d79afe98e705c5f244fd56ad991ca63576a54
Alexis <alexis.martinho.78@gmail.com>
2025-03-12 11:19:20 +0100

    Move Variables from GameMode to Spawner

--- Source/FastArenaShooter/Private/FASGameMode.cpp
+++ Source/FastArenaShooter/Private/FASGameMode.cpp
@@ -17,42 +17,6 @@ void AFASGameMode::BeginPlay()
 	Super::BeginPlay();
 	
 	bIsPlayerDead = false;
-
-	UGameplayStatics::GetAllActorsOfClass(GetWorld(), AFASSpawnerEnemyBase::StaticClass(), SpawnerEnemy);
-	PassVariableToSpawners();
-}
-
-void AFASGameMode::PassVariableToSpawners()
-{
-	if (bIsPlayerDead) return;
-	
-	for (AActor* Actor : SpawnerEnemy)
-	{
-		AFASSpawnerEnemyBase* Spawner = Cast<AFASSpawnerEnemyBase>(Actor);
-		if (Spawner)
-		{
-			switch (Spawner->EnemyType)
-			{
-				case None:
-					UE_LOG(LogTemp, Error, TEXT("No Type was chosen for spawner : %s"), *Spawner->GetName());
-					FMessageLog(FName("PIE")).Error(FText::FromString(FString::Printf(TEXT("No Type was chosen for spawner: %s"), *Spawner->GetName())));
-					break;
-				
-				case EnemyType1:
-					Spawner->SpawnSettingsEnemy = SpawnSettingsEnemy1;
-					break;
-				
-				case EnemyType2:
-					Spawner->SpawnSettingsEnemy = SpawnSettingsEnemy2;
-					break;
-				
-				default:
-					UE_LOG(LogTemp, Error, TEXT("No Type was chosen for spawner : %s"), *Spawner->GetName());
-					FMessageLog(FName("PIE")).Error(FText::FromString(FString::Printf(TEXT("No Type was chosen for spawner: %s"), *Spawner->GetName())));
-					break;
-			}
-		}
-	}
 }
 
 bool AFASGameMode::CheckIsPlayerDead()
